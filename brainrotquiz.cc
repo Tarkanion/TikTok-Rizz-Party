a5e099654392386dd08bc96f6707a48d43f936f9
Cohwyn Cheng <1203318@learn.vsb.bc.ca>
2024-06-20 11:30:33 -0700

    added second question

--- brainrotquiz.cc
+++ brainrotquiz.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 	string s;
-	cout<<"Brainrot Quiz! Who is the group leader?>"<<endl;
+	cout<<"Brainrot Quiz! Who is the group leader?"<<endl;
 	getline(cin,s);
 	if(s=="blue tie kid"){
 		cout << "correct" << endl;
@@ -11,5 +11,14 @@ int main(){
 	else{
 		cout<<"WRONG"<<endl;
 	}
+
+	cout<<"whos the founder of brainrot?"<<endl;
+	getline(cin,s);
+	if(s=="turkish quandale dingle"){
+		cout<<"correct"<<endl;
+	}
+	else{
+		cout<<"wrong"<<endl;
+	}	
 	return 0;
 }
